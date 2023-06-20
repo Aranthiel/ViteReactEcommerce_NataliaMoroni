41dd00cfd4248913c7cbb088df0eb5fb9dbaa25c
Natalia Moroni <nmoronidamasso@gmail.com>
2023-06-20 13:35:49 -0300

    Solucionado el filtrado gracias a Nicolás de CoderAsk

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState}from 'react';
 //imports librerias extrernas
-import { Link } from 'react-router-dom';
+
 //imports propios
 import "./ItemList.css"
 import ProductCard from "../ProductCard/ProductCard";
@@ -16,31 +16,26 @@ const ItemList = (categoryId) => {
     useEffect(()=> {
         fetch(API_URL)
         .then(response => response.json())
-        .then(json => {setProducts(json.products)})
+        .then(json => {
+            const arrayRespuesta = json.products;
+            if (categoryId.category !== undefined){
+                // si tenemos que mostrar categoría, filtramos el array y seteamos el array filtrado
+                const arrayFiltrado = arrayRespuesta.filter(producto => producto.category === categoryId.category)
+                setProducts(arrayFiltrado)
+            } else {
+                // sino, seteamos el array entero
+                setProducts(arrayRespuesta)
+            }  
+        })
         .catch(error => {
             console.error('Error fetching products:', error);
         });
-    }, []);
-    console.log("categoryId",categoryId)
-    
-    const filtrados = products.filter(producto => producto.category === categoryId.category);  
-    console.log("products",products)
-    console.log("filtrados", filtrados)
-    let iaestoyharta=[];
-
-    if (categoryId.category !== undefined){
-        console.log("categoryId tiene valor y deberian renderizarse SOLAMENTE los  productos de la categoria");
-        iaestoyharta=[].concat(filtrados)
-        
-    } else {
-        console.log("categoryId NO  tiene valor y deberian renderizarse TODOS los productos");
-        iaestoyharta=[].concat(products)
-    }
-    console.log("iaestoyharta", iaestoyharta)
+    }, [categoryId]);
+   
 
     return (
         <div className="cards-list margin05rem">
-            {iaestoyharta.map((producto)=>{ 
+            {products.map((producto)=>{ 
                 return (<ProductCard key={producto.id} item={producto} className="heigt40rem"/>)
             })}
         </div>
