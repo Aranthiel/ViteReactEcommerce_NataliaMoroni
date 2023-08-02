fc21d5fb48a712d4990654e56edc64620e77fae8
Natalia Moroni <nmoronidamasso@gmail.com>
2023-08-02 00:28:18 -0300

    deploy 4 base:'https://Aranthiel.github.io/ViteReactEcommerce_NataliaMoroni'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base:'https://Aranthiel.github.io/ViteReactEcommerce_NataliaMoroni'
+  base:'https://Aranthiel.github.io/ViteReactEcommerce_NataliaMoroni'
 })
